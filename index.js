692c5af563e2518d1702173d057f08d509b79a12
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-03-14 14:46:50 +0900

    Prettier codes with ESLint, Prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const enyg = require('./dist/enyg.js');
-const hangulDecompose = require('./dist/hangulDecompose.js');
-const to = require('./dist/to.js');
+const enyg = require("./dist/enyg.js");
+const hangulDecompose = require("./dist/hangulDecompose.js");
+const to = require("./dist/to.js");
 
 module.exports = Object.assign(enyg, {
   hangulDecompose,
