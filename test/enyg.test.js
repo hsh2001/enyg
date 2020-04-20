97dabf66c2c92a739852a8a2846fa8c0dadda794
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-04-20 17:22:31 +0900

    Use jest

---
+++ test/enyg.test.js
@@ -0,0 +1,25 @@
+// import enyg, { hangulDecompose } from '../index';
+
+const enyg = require('../index.js');
+const { hangulDecompose, toChosung } = enyg;
+const testExpectResult = require('./hangul-decompose-result');
+
+describe('은는이가.js 테스트', () => {
+  test('조사 선택', () => {
+    expect(
+      enyg(
+        '다람쥐[은|는] 헌 쳇바퀴[이|가] 타고파. 나[은|는] 너[을|를] 좋아해.',
+      ),
+    ).toBe('다람쥐는 헌 쳇바퀴가 타고파. 나는 너를 좋아해.');
+  });
+
+  test('한글 분해', () => {
+    expect(
+      Array.from('가나다핳헿쀍뾹뀱ㅔㅋ').map(hangulDecompose),
+    ).toStrictEqual(testExpectResult);
+  });
+
+  test('초성 가져오기', () => {
+    expect(['가을', '하늘'].map(toChosung)).toStrictEqual(['ㄱㅇ', 'ㅎㄴ']);
+  });
+});
---
+++ test/hangul-decompose-result.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    char: '가',
+    chosung: 'ㄱ',
+    jungsung: 'ㅏ',
+    jongsung: '',
+  },
+  {
+    char: '나',
+    chosung: 'ㄴ',
+    jungsung: 'ㅏ',
+    jongsung: '',
+  },
+  {
+    char: '다',
+    chosung: 'ㄷ',
+    jungsung: 'ㅏ',
+    jongsung: '',
+  },
+  {
+    char: '핳',
+    chosung: 'ㅎ',
+    jungsung: 'ㅏ',
+    jongsung: 'ㅎ',
+  },
+  {
+    char: '헿',
+    chosung: 'ㅎ',
+    jungsung: 'ㅔ',
+    jongsung: 'ㅎ',
+  },
+  {
+    char: '쀍',
+    chosung: 'ㅃ',
+    jungsung: 'ㅞ',
+    jongsung: 'ㄺ',
+  },
+  {
+    char: '뾹',
+    chosung: 'ㅃ',
+    jungsung: 'ㅛ',
+    jongsung: 'ㄺ',
+  },
+  {
+    char: '뀱',
+    chosung: 'ㄲ',
+    jungsung: 'ㅠ',
+    jongsung: 'ㄺ',
+  },
+  {
+    char: 'ㅔ',
+  },
+  {
+    char: 'ㅋ',
+  },
+];
